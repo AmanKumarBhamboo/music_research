4da8c00fbda555f2029d86d6215c0920c9ad3494
Drazo <amankumarbhamboo14022005@gmail.com>
2026-02-24 09:24:26 +0530

    ALMIGHTY PUSH NO. 2

---
+++ dataset_without_GPU_usage/database_management_part.sql
@@ -0,0 +1,4 @@
+SELECT schema_name
+FROM information_schema.schemata;
+CREATE SCHEMA IF NOT EXISTS songs_metadata;
+-- Aryan bhai isme abb table bana ke kar dena puri update maine dedi hai updates.txt mai
